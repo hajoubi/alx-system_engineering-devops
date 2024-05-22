7c34c849698c3b6f45d7ddaf77a9b638d93f6a49
hajoubi <hajoubi.mohammed2@gmail.com>
2024-05-22 01:43:16 +0100

    fixssss

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
-""" Rest API script that gathers data from an API. """
-import json
-import sys
-import urllib
-import urllib.request
+"""
+Module 0-gather_data_from_an_API:
 
-# The base API url for getting the employee object
-USER_API_URL = "https://jsonplaceholder.typicode.com/users/"
+Using 'https://jsonplaceholder.typicode.com', for a given employee ID,
+returns information about his/her TODO list progress.
+"""
 
-# The base API url for getting all todo objects for an employee
-TODO_API_URL = "https://jsonplaceholder.typicode.com/todos?userId="
+import requests
+from sys import argv
 
-# Employee ID passed as an argument to the script
-emp_id: str = sys.argv[1] if len(sys.argv) > 1 else ""
-
-# Get all the todos for a given employee ID
-if emp_id.isdigit():
-    try:
-        user_url = f"{USER_API_URL}{emp_id}"
-        todos_url = f"{TODO_API_URL}{emp_id}"
-
-        emp_response = urllib.request.urlopen(user_url)
-        todos_response = urllib.request.urlopen(todos_url)
-
-        emp_data = emp_response.read()
-        todos_data = todos_response.read()
-
-        employee = json.loads(emp_data)
-        todos = json.loads(todos_data)
-
-        name = employee.get("name")
-        done = len([todo for todo in todos if todo.get("completed")])
-        total = len(todos)
-
-        print(f"Employee {name} is done with tasks({done}/{total}):")
-        for todo in todos:
-            if todo.get("completed"):
-                print(f"\t {todo.get('title')}")
-
-    except urllib.error.URLError as err:
-        print(f"An error occurred: {err}")
-    except json.JSONDecodeError as err:
-        print(f"Error decoding JSON: {err}")
+if __name__ == "__main__":
+    if len(argv) == 2:
+        SRC = 'https://jsonplaceholder.typicode.com/'
+        id = int(argv[1])
+        res_user = requests.get('{}users/{}'.format(SRC, id)).json()
+        res_todos = requests.get('{}todos?userId={}'.format(SRC, id)).json()
+        name = res_user.get('name')
+        todos = list(res_todos)
+        todos_done = list(filter(lambda x: x.get('completed'), todos))
+        print(
+            'Employee {} is done with tasks({}/{}):'.format(
+                name,
+                len(todos_done),
+                len(todos))
+        )
+        for todo_done in todos_done:
+            print('\t {}'.format(todo_done.get('title')))
